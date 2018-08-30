eb320850b758490a51de7398d761e462249b75ae
Gunnar Rolander <gunnar.rolander@gmail.com>
2018-08-30 12:37:29 +0200

    Made seeds to simulate game start

--- mordheim/db/seeds.rb
+++ mordheim/db/seeds.rb
@@ -19,13 +19,18 @@ p2.save
 
 p3 = Place.create(namn: 'Testarea 3', beskrivning: 'Testest', warband: w2, lat: -150, lng: 134)
 p4 = Place.create(namn: 'Testarea 4', beskrivning: 'Testest', warband: w2, lat: -128, lng: 168)
+w2.visited_places << p3
+w2.visited_places << p4
+w2.visited_places << p2
+
 
 p.linked_places << [p4, p2]
 p2.linked_places << [p, p3]
 w.visited_places << p
 p.save
 
-t = Turn.create(nummer: 1, fas: "Strid")
+t = Turn.create(nummer: 1, fas: "Ordergivning")
+=begin
 t2 = Turn.create(nummer: 2, fas: "Order")
 b = Battle.create(place: p2, turn: t2)
 b.warbands << [w, w2]
@@ -38,5 +43,7 @@ b3.warbands << [w, w2]
 b3.save
 a = Action.create(turn: t2, warband: w, place: p)
 a2 = Action.create(turn: t, warband: w2, place: p)
+=end
+
 
 
