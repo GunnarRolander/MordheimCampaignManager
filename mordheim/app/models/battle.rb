6a42606d4a1aae7f6236938f451332ebfb22dfa1
Gunnar Rolander <gunnar.rolander@gmail.com>
2018-11-28 18:59:28 +0100

    Updated retreat-functionality

--- mordheim/app/models/battle.rb
+++ mordheim/app/models/battle.rb
@@ -1,3 +1,4 @@
+require 'set'
 class Battle < ApplicationRecord
   has_and_belongs_to_many :warbands
   belongs_to :winner, :class_name => 'Warband', optional: true
@@ -5,6 +6,30 @@ class Battle < ApplicationRecord
   belongs_to :turn
 
   def possible_retreats
-    self.place.linked_places.where(warband: self.place.warband)
+    retreats = self.place.linked_places.where(warband: self.place.warband)
+
+    if retreats.empty?
+      examined_places = self.place.linked_places.to_set
+      places_to_examine = examined_places
+      examined_places << self.place
+
+      while(true)
+        places_to_examine = places_to_examine.map{ |place| place.linked_places }.flatten.to_set      
+        places_to_examine = places_to_examine - examined_places
+        break if places_to_examine.empty?
+
+        controlled_retreats = places_to_examine.keep_if {| place | place.warband == self.place.warband}
+        
+        unless controlled_retreats.empty?
+          random_place = controlled_retreats.to_a.sample
+          retreats = [random_place]
+          break
+        end
+
+        examined_places << places_to_examine
+      end
+
+    return retreats
+    end
   end
 end
