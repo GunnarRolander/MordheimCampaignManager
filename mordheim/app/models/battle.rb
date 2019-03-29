06913613b4bcd9c8fbafdfe192f464666c28be3d
Gunnar Rolander <gunnar.rolander@gmail.com>
2019-03-29 12:35:37 +0100

    Om man utplånas flyr man till närmsta tomma område.

--- mordheim/app/models/battle.rb
+++ mordheim/app/models/battle.rb
@@ -7,29 +7,36 @@ class Battle < ApplicationRecord
 
   def possible_retreats
     retreats = self.place.linked_places.where(warband: self.place.warband)
+    retreats = find_closest_place_belonging_to_warband(self.place.warband) if retreats.empty?
+    retreats = find_closest_place_belonging_to_warband(nil) if retreats.empty?
+    return retreats
+  end
 
-    if retreats.empty?
-      examined_places = self.place.linked_places.to_set
-      places_to_examine = examined_places
-      examined_places << self.place
+  private
+  def find_closest_place_belonging_to_warband(warband)
+    examined_places = [self.place].to_set
+    places_to_examine = examined_places
 
-      while(true)
-        places_to_examine = places_to_examine.map{ |place| place.linked_places }.flatten.to_set      
-        places_to_examine = places_to_examine - examined_places
-        break if places_to_examine.empty?
+    while(true && i)
+      places_to_examine = places_to_examine.map{ |place| place.linked_places }.flatten.to_set
+      places_to_examine = places_to_examine.subtract(examined_places)
 
-        controlled_retreats = places_to_examine.keep_if {| place | place.warband == self.place.warband}
-        
-        unless controlled_retreats.empty?
-          random_place = controlled_retreats.to_a.sample
-          retreats = [random_place]
-          break
-        end
-
-        examined_places << places_to_examine
+      if places_to_examine.empty?
+        closest_place = []
+        break
       end
 
+      controlled_retreats = places_to_examine.select {| place | place.warband == warband || place.warband_id == warband}
+
+      unless controlled_retreats.empty?
+        random_place = controlled_retreats.to_a.sample
+        closest_place = [random_place]
+        break
+      end
+
+      examined_places.merge( places_to_examine )
     end
-    return retreats
+
+    return closest_place
   end
 end
